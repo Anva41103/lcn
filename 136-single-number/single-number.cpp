fadebfe14c8be346d41568f75ef0209eaa81783d
Anva41103 <146993960+Anva41103@users.noreply.github.com>
2024-06-05 01:59:17 +0530

    Time: 7 ms (96.24%) | Memory: 19.2 MB (84.82%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) 
+    {
+        int x=0;
+        for(vector<int>:: iterator it= nums.begin(); it!=nums.end();it++)
+        {
+            x=x^*(it);
+        }
+        return x;
+    }
+};
