86444020e97452fe65c3aa58a77c90176f49c7ba
Ananya Srivastava <146993960+Anva41103@users.noreply.github.com>
2024-07-23 01:35:15 +0530

    Time: 22 ms (88.63%) | Memory: 24.1 MB (79.22%) - LeetSync

--- 2502-sort-the-people/sort-the-people.cpp
+++ 2502-sort-the-people/sort-the-people.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
-    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
-        vector<int> indices(heights.size());
-        iota(indices.begin(), indices.end(), 0);
-        sort(indices.begin(), indices.end(), [&](int i, int j) { return heights[i] > heights[j]; });
-        vector<string> sortedNames(indices.size());
-        transform(indices.begin(), indices.end(), sortedNames.begin(), [&](int i) { return names[i]; });
-        return sortedNames;
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) 
+    {
+        vector <int> ind(names.size());
+        for(int i=0;i<names.size();i++)
+        {
+            ind[i]=i;
+        }
+        sort(ind.begin(),ind.end(), [&heights](int a,int b){return heights[a]>heights[b];});
+        vector <string> k;
+        for(int i=0;i<names.size();i++)
+        {
+            k.push_back(names[ind[i]]);
+        }
+        return k;
     }
 };
