a050e183ec70865e9e1dc384874d9afa1cf61e8a
Ananya Srivastava <146993960+Anva41103@users.noreply.github.com>
2024-10-16 01:47:26 +0530

    Time: 35 ms (42.12%) | Memory: 15.7 MB (44.92%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
@@ -3,11 +3,10 @@ public:
     long long minimumSteps(string s) {
         long long swap = 0;
         int black = 0; 
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == '0') 
-                swap += (long long) black; 
-            else
-                black++; 
+        for(int i=s.length()-1;i>=0;i--)
+        {
+            if(s[i]=='0') black++;
+            else swap+=black;
         }
         return swap;
     }
